35350400e64d1bcc481fd08d691c522b49adcc27
Mirosław Gucwa <mgucwa@gmail.com>
2022-06-24 14:22:36 +0200

    Server - step 3

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 const app = express();
 
+app.use('/user', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
 app.use((req, res, next) => {
   res.show = (name) => {
     res.sendFile(path.join(__dirname, `/views/${name}`));
@@ -16,25 +20,17 @@ app.get('/', (req, res) => {
   res.show('index.html');
 });
 
+app.get('/home', (req, res) => {
+  res.show('index.html');
+});
+
 app.get('/about', (req, res) => {
   res.show('about.html');
 });
 
-app.get('/contact', (req, res) => {
-  res.show('contact.html');
-});
-
-app.get('/info', (req, res) => {
-  res.show('info.html');
-});
-
-app.get('/history', (req, res) => {
-  res.show('history.html');
-});
-
-app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '/style.css'));
-});
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+})
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
