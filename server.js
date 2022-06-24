3dbc15a0cd807b45dd1efaced42beca794cddcc9
Mirosław Gucwa <mgucwa@gmail.com>
2022-06-24 13:53:19 +0200

    Serwer step 2 - with Kodilla

--- server.js
+++ server.js
@@ -3,29 +3,37 @@ const path = require('path');
 
 const app = express();
 
-// app.use('/admin', (req, res, next) => {
-//   if (isAdmin) next();
-//   else res.send('Go away!');
-// });
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/index.html'));
+  res.show('index.html');
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/about.html'));
+  res.show('about.html');
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/contact.html'));
+  res.show('contact.html');
 });
 
 app.get('/info', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/info.html'));
+  res.show('info.html');
 });
 
 app.get('/history', (req, res) => {
-  res.sendFile(path.join(__dirname, '/views/history.html'));
+  res.show('history.html');
+});
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname, '/style.css'));
 });
 
 app.listen(8000, () => {
