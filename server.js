855ddec7af3e43e634a9c3e87bd5a91c08181030
Mirosław Gucwa <mgucwa@gmail.com>
2022-06-27 23:20:56 +0200

    With middleware set, show...

--- server.js
+++ server.js
@@ -1,35 +1,53 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use('/user', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '/views/login.html'));
+app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'main' }));
+// app.engine('.hbs', hbs());
+// app.set('view engine', '.hbs');
+app.set('view engine', 'hbs');
+
+// app.get('/hello/:name', (req, res) => {
+//   res.send(`Hello ${req.params.name}`);
+// });
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
+  // template: ./views/hello.hbs
 });
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
+app.use('/user', (req, res, next) => {
+  // res.sendFile(path.join(__dirname, '/views/login.html'));
+  // res.render('login', { layout: false });
+  res.render('login');
 });
+
+// app.use((req, res, next) => {
+//   res.show = (name) => {
+//     res.sendFile(path.join(__dirname, `/views/${name}`));
+//   };
+//   next();
+// });
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/home', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+  // res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+  if (res.status(404)) res.render('404');
 })
 
 app.listen(8000, () => {
